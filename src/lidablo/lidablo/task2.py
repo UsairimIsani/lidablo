f51d57db6fb921e0bbd1e0ac725bc33dfb0a2d20
Usairim Isani <usairim.isani@gmail.com>
2022-09-21 11:58:52 +0300

    line detection using groups.

--- src/lidablo/lidablo/task2.py
+++ src/lidablo/lidablo/task2.py
@@ -42,16 +42,16 @@ class FeatureExtracter(Node):
 
     #     return points
 
-    def polar_to_cartesian_coordinate(self, ranges, angle_min, angle_max, angle_increment, msg):
+    def polar_to_cartesian_coordinate(self, msg):
 
-        angle = angle_min               # start angle
+        angle = msg.angle_min               # start angle
         tf_points = []
-        for range in ranges:
+        for range in msg.ranges:
             x = range * np.cos(angle)
             y = range * np.sin(angle)
 
             # current angle is last angle add angle_increment
-            angle += angle_increment
+            angle += msg.angle_increment
             tf_points.append([x, y])
 
         # transform the transformed points to numpy array
@@ -67,8 +67,7 @@ class FeatureExtracter(Node):
         step_size = 5
         accepted_error_rad = 0.1745
         angles_and_errors = []
-        ranges = self.polar_to_cartesian_coordinate(
-            msg.ranges, msg.angle_min, msg.angle_max, msg.angle_increment, "Detecting Corners.")
+        ranges = self.polar_to_cartesian_coordinate(msg)
 
         for idx, _ in enumerate(ranges):
             last_index = idx + window_size
@@ -105,76 +104,60 @@ class FeatureExtracter(Node):
         plt.savefig(f"img/corners-{msg}")
 
     def plot_lines(self, lines, msg):
-        # plt.figure()
+        plt.figure()
+        print("Line length : ", len(lines))
         for line in lines:
-            print("Line length : ", len(line))
-            # if len(line) > 0:
-            # plt.scatter(line[:, 0], line[:, 1])
-            # plt.savefig(f"img/lines-{msg}")
+            if len(line) > 0:
+                # print(line)
+                line = np.array(line)
+                # plt.plot(line[:, 0], line[:, 1])
+                plt.scatter(line[:, 0], line[:, 1])
+        plt.savefig(f"img/lines-{msg}")
 
     def detect_lines(self, msg):
         total_ranges = len(msg.ranges) - 1
+        ranges = self.polar_to_cartesian_coordinate(msg)
 
-        ranges = self.polar_to_cartesian_coordinate(
-            msg.ranges, msg.angle_min, msg.angle_max, msg.angle_increment, "Detecting Corners.")
-
-        # for point in ranges:
-        #     x = []
-        #     y = []
-        # line = np.polyfit(x, y, 1)
-
-        accepted_error = 15
-        previous_slope = 0
+        window = 10
+        accepted_error = 3
         line = []
         lines = []
-        for idx, _ in enumerate(ranges):
-            next_idx = idx + 1
-            if next_idx < total_ranges:
+        for idx, _ in enumerate(ranges, window):
+            next_idx = idx + window
+            if next_idx + window < total_ranges:
 
-                current_point = ranges[idx]
-                next_point = ranges[next_idx]
+                current_point_group = ranges[idx:idx+window]
+                next_point_group = ranges[next_idx:next_idx+window]
 
-                current_slope = next_point[0] - current_point[0] / \
-                    next_point[1] - current_point[1]
+                current_poly = np.polyfit(
+                    current_point_group[:, 0], current_point_group[:, 1], 1)
 
-                slope_diff = np.abs(current_slope - previous_slope)
+                next_poly = np.polyfit(
+                    next_point_group[:, 0], next_point_group[:, 1], 1)
 
-                # print("Slope Diff  ", slope_diff)
+                calculated_err = np.abs(next_poly[0]) - np.abs(current_poly[0])
 
-                if (slope_diff <= accepted_error):
-                    line.append(current_point)
+                print("next_poly error Diff  ", next_poly)
+                print("current_poly error Diff  ", current_poly)
+
+                if (calculated_err <= accepted_error):
+                    line.extend(current_point_group)
                 else:
-                    # if len(line) > 0:
                     lines.append(line)
                     line = []
+                    line.extend(current_point_group)
 
-                previous_slope = current_slope
-        print("Number of Lines ", len(lines))
-        lines = np.array(lines, dtype=object)
+                # previous_slope = current_poly
+        # print("Number of Lines ", len(lines))
         return lines
 
     def scan_callback(self, msg):
 
-        # self.polar_to_cartesian_coordinate(
-        # msg.ranges, msg.angle_min,
-        # msg.angle_max, msg.angle_increment)
-
-        #################################
-        ranges = []
-        for r in msg.ranges:
-            if r > 2.5 or r < 1.0:   # Feature extraction code here
-                ranges.append(0.0)
-            else:
-                ranges.append(r)
-
-        ##################################
-
-        # line = self.detect_lines(msg)
-        # print(line)
-
         corners = self.detect_corners(msg)
         lines = self.detect_lines(msg)
         self.plot_corners(corners, "Detecting Corners")
+        self.plot_corners(
+            self.polar_to_cartesian_coordinate(msg), "Range")
         self.plot_lines(lines, "Detecting Lines")
 
         scan = LaserScan()
@@ -186,7 +169,7 @@ class FeatureExtracter(Node):
         scan.time_increment = msg.time_increment
         scan.range_min = msg.range_min
         scan.range_max = msg.range_max
-        scan.ranges = ranges
+        scan.ranges = msg.ranges
         self.publisher_.publish(scan)
 
         self.scan_idx += 1
